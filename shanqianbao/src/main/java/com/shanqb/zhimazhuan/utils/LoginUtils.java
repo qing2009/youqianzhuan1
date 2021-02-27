285b2da35eb0e431b33b7d1c49afb712fc53005c
liuxin <471823471@qq.com>
2021-02-27 13:25:33 +0800

    修改最低提现金额字段

--- shanqianbao/src/main/java/com/shanqb/zhimazhuan/utils/LoginUtils.java
+++ shanqianbao/src/main/java/com/shanqb/zhimazhuan/utils/LoginUtils.java
@@ -25,7 +25,7 @@ public class LoginUtils {
         editor.putString(SharedPreConstants.allAmt, loginResponse.getData().getAllAmt() + "");
         editor.putString(SharedPreConstants.txAmt, loginResponse.getData().getTxAmt() + "");
         editor.putString(SharedPreConstants.shareCode, loginResponse.getData().getShareCode() + "");
-        editor.putString(SharedPreConstants.txHaveAmt, loginResponse.getData().getTxHaveAmt());
+        editor.putString(SharedPreConstants.txMinAmt, loginResponse.getData().getTxMinAmt());
         String channelListJson = new Gson().toJson(loginResponse.getData().getListbc());
         editor.putString(SharedPreConstants.channelList, channelListJson);
         editor.commit();
