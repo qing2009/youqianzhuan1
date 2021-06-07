3a33618f04e6a14dabc5b3abc78f48cb498ca883
liuxin <lean5c@qq.com>
2021-06-07 14:00:43 +0800

    以01-base为分支创建的分支。 修改内容： 1.修改app名称为“鼎耀联盟”，修改app主色调，修改包名为“dingyaolianmeng”。 2.修改了商户号。 3.修改赚钱tab页图片

---
+++ shanqianbao/src/main/java/com/shanqb/dingyaolianmeng/activity/MyBaseActivity.java
@@ -0,0 +1,100 @@
+package com.shanqb.dingyaolianmeng.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.shanqb.dingyaolianmeng.R;
+import com.shanqb.dingyaolianmeng.inter.MyQueueResponse;
+import com.shanqb.dingyaolianmeng.utils.MyVolleyUtils;
+import com.shanqb.dingyaolianmeng.utils.NetworkUtils;
+import com.shanqb.dingyaolianmeng.utils.XToastUtils;
+import com.shanqb.dingyaolianmeng.view.LoadingProgressDialog;
+
+import java.util.Map;
+
+import butterknife.ButterKnife;
+
+public abstract class MyBaseActivity extends AppCompatActivity{
+
+    public MyVolleyUtils myVolley = null;
+    public RequestQueue mQueue = null;
+
+    private LoadingProgressDialog mLoadingProgressDialog;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mLoadingProgressDialog = new LoadingProgressDialog(this);
+        myVolley = new MyVolleyUtils(this);
+        mQueue  = myVolley.getmQueue();
+
+        initLayout();
+        ButterKnife.bind(this);
+
+        initBundleData();
+        initData();
+        initWeight();
+    }
+
+    /** 初始化布局 */
+    public abstract void initLayout();
+
+    /** 初始化绑定数据 */
+    public abstract void initBundleData();
+
+    /** 初始化数据 */
+    public abstract void initData();
+
+    /** 初始化控件 */
+    public abstract void initWeight();
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        dismissLoadingDialog();
+        mLoadingProgressDialog = null;
+    }
+
+
+    /**
+     * 显示dialog
+     */
+    protected void showLoadingDialog() {
+        if (!isFinishing() && mLoadingProgressDialog != null && !mLoadingProgressDialog.isShowing()) {
+            mLoadingProgressDialog.show();
+        }
+    }
+
+    /**
+     * 关闭dialog
+     */
+    protected void dismissLoadingDialog() {
+        if (!isFinishing() && mLoadingProgressDialog != null && mLoadingProgressDialog.isShowing()) {
+            mLoadingProgressDialog.dismiss();
+        }
+    }
+
+
+    protected void requestPostQueue(boolean isShowDialog,String requestAction, Map<String, String> requestParams, MyQueueResponse myQueueResponse){
+        try {
+            if (!NetworkUtils.checkNetworkConnectionState(this)) {//未连接到网络
+                XToastUtils.toast(getString(R.string.net_close));
+                return;
+            }
+            if (isShowDialog){
+                showLoadingDialog();
+            }
+            myVolley.requestPostQueue(requestAction,requestParams,myQueueResponse);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            dismissLoadingDialog();
+        }
+    }
+
+}
---
+++ shanqianbao/src/main/java/com/shanqb/dingyaolianmeng/utils/sdk/XianWangUtils.java
@@ -0,0 +1,50 @@
+package com.shanqb.dingyaolianmeng.utils.sdk;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+
+import com.pceggs.workwall.util.PceggsWallUtils;
+import com.shanqb.dingyaolianmeng.utils.DeviceUtils;
+import com.shanqb.dingyaolianmeng.utils.SharedPreConstants;
+import com.shanqb.dingyaolianmeng.utils.SharedPreferencesUtil;
+
+/**
+ * 接入享玩sdk使用的工具类
+ */
+public class XianWangUtils {
+
+    /**
+     * 接入享玩sdk使用的常量
+     */
+//    public static final String XIANWANG_PID = "11495";//渠道ID(对接渠道享玩提供)
+//    public static final String XIANWANG_APPKEY = "PCDDXW8_YQZ_11495";//渠道key(对接渠道享玩提供)
+
+
+
+    public static void startSDK(Activity context,String channelUser,String channelKey){
+        String deviceId = DeviceUtils.getDeviceId(context);
+        String xwdeviceid = getXwdeviceid(context);
+        String merCode = SharedPreferencesUtil.getStringValue(context, SharedPreConstants.merCode, "");
+
+//        PceggsWallUtils.loadAd(context, XIANWANG_PID, XIANWANG_APPKEY, merCode, deviceId, xwdeviceid);
+        PceggsWallUtils.loadAd(context, channelUser, channelKey, merCode, deviceId, xwdeviceid);
+//      PceggsWallUtils.loadAd(context, "10001", "PCDDXW_CS_10001", userid + "", deviceId, xwdeviceid);
+    }
+
+
+
+    public static String getXwdeviceid(Context context) {
+        String xwdeviceid = "";
+
+        if (Build.VERSION.SDK_INT <= 28) {
+            xwdeviceid =  DeviceUtils.getIMEI(context, 0) + "," +  DeviceUtils.getIMEI(context, 1) + "," +  DeviceUtils.getMEID(context);
+        } else {
+            xwdeviceid = SharedPreferencesUtil.getStringValue(context, SharedPreConstants.OAID,"");
+        }
+
+        return xwdeviceid;
+    }
+
+}
