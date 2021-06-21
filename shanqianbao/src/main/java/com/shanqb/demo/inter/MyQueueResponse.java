30a4071a380163549f7336cfb2428c7e52e8842a
Administrator <test>
2021-06-21 17:45:08 +0800

    git忽略配置文件更新

--- shanqianbao/src/main/java/com/shanqb/demo/inter/MyQueueResponse.java
+++
@@ -1,8 +0,0 @@
-package com.shanqb.demo.inter;
-
-import com.android.volley.VolleyError;
-
-public interface MyQueueResponse {
-    void onResponse(String actonString, String response);
-    void onErrorResponse(String actonString, VolleyError error);
-}
