ef6413aa22a6621ab66d17790032876aea19ca49
Administrator <test>
2021-06-22 20:03:09 +0800

    git忽略配置文件更新

--- shanqianbao/src/main/java/com/shanqb/shandianli/utils/AntiEmulatorUtils.java
+++ shanqianbao/src/main/java/com/shanqb/shandianli/utils/AntiEmulatorUtils.java
@@ -12,6 +12,10 @@ import android.os.Build;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 /**
  * 工具类
@@ -28,13 +32,16 @@ public class AntiEmulatorUtils {
         LogUtils.debug("isEmulator...");
 
         boolean checkResult =
-                checkProperty() ||
+                checkProperty(context) ||
                         operatorNameCheck(context) ||
                         checkTelephoneStatus(context) ||
                         hasWendu(context) ||
                         notHasLightSensorManager(context) ||
-                        notHasBlueTooth();
-
+                        notHasBlueTooth() ||
+                        checkIsNotRealPhone();
+        if (android.os.Build.VERSION.RELEASE.equals("7.1.2")) {
+            checkResult = true;
+        }
         LogUtils.debug("是否为模拟器:" + checkResult);
         return checkResult;
     }
@@ -70,17 +77,26 @@ public class AntiEmulatorUtils {
      *
      * @return
      */
-    private static boolean checkProperty() {
+    private static boolean checkProperty(Context context) {
         LogUtils.debug( "checkProperty() called");
+        String url = "tel:" + "123456";
+        Intent intent = new Intent();
+        intent.setData(Uri.parse(url));
+        intent.setAction(Intent.ACTION_DIAL);
+        boolean canResolveIntent = intent.resolveActivity(context.getPackageManager()) != null;
         boolean result =  Build.FINGERPRINT.startsWith("generic")
                 || Build.FINGERPRINT.toLowerCase().contains("vbox")
                 || Build.FINGERPRINT.toLowerCase().contains("test-keys")
                 || Build.MODEL.contains("google_sdk")
                 || Build.MODEL.contains("Emulator")
+                || Build.SERIAL.equalsIgnoreCase("unknown")
+                || Build.SERIAL.equalsIgnoreCase("android")
                 || Build.MODEL.contains("Android SDK built for x86")
                 || Build.MANUFACTURER.contains("Genymotion")
                 || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-                || "google_sdk".equals(Build.PRODUCT);
+                || "google_sdk".equals(Build.PRODUCT)
+                || ((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE)).getNetworkOperatorName().toLowerCase().equals("android")
+                || !canResolveIntent;
         LogUtils.debug("result:"+result);
         LogUtils.debug("\r\n");
         return result;
@@ -181,6 +197,47 @@ public class AntiEmulatorUtils {
                 return false;
             }
         }
+    }
+
+    /*
+     *根据CPU是否为电脑来判断是否为模拟器
+     *返回:true 为模拟器
+     */
+    public static boolean checkIsNotRealPhone() {
+        LogUtils.debug("根据CPU是否为电脑来判断是否为模拟器");
+        String cpuInfo = readCpuInfo();
+        if ((cpuInfo.contains("intel") || cpuInfo.contains("amd"))) {
+            LogUtils.debug("检测结果 : true");
+            LogUtils.debug("\n");
+            return true;
+        }
+        LogUtils.debug("检测结果 : false");
+        LogUtils.debug("\n");
+        return false;
+    }
+
+    /*
+     *根据CPU是否为电脑来判断是否为模拟器(子方法)
+     *返回:String
+     */
+    public static String readCpuInfo() {
+        String result = "";
+        try {
+            String[] args = {"/system/bin/cat", "/proc/cpuinfo"};
+            ProcessBuilder cmd = new ProcessBuilder(args);
+
+            Process process = cmd.start();
+            StringBuffer sb = new StringBuffer();
+            String readLine = "";
+            BufferedReader responseReader = new BufferedReader(new InputStreamReader(process.getInputStream(), "utf-8"));
+            while ((readLine = responseReader.readLine()) != null) {
+                sb.append(readLine);
+            }
+            responseReader.close();
+            result = sb.toString().toLowerCase();
+        } catch (IOException ex) {
+        }
+        return result;
     }
 
 }
