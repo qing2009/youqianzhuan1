1d393dd5074a34d27c3f0aa688b5687076cd8da4
Administrator <test>
2021-09-25 17:15:23 +0800

    jux

---
+++ shanqianbao/src/main/java/com/shanqb/haoyi/adapter/QiandaoRecordAdapter.java
@@ -0,0 +1,52 @@
+package com.shanqb.haoyi.adapter;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.shanqb.haoyi.R;
+import com.shanqb.haoyi.bean.QiandaoItemBean;
+import com.shanqb.haoyi.test.BaseRecyclerViewAdapter;
+
+import java.util.List;
+
+public class QiandaoRecordAdapter extends BaseRecyclerViewAdapter {
+
+    private List<QiandaoItemBean> qiandaoItemBeanList;
+    /**
+     * 默认加载图片
+     */
+    private ColorDrawable mColorDrawable;
+
+    public QiandaoRecordAdapter(List<QiandaoItemBean> qiandaoItemBeanList) {
+        super(R.layout.item_layout_qiandao);
+        this.qiandaoItemBeanList = qiandaoItemBeanList;
+        mColorDrawable = new ColorDrawable(Color.parseColor("#555555"));
+    }
+
+    @Override
+    public void onBindViewData(ViewHolder var1, int var2) {
+        try {
+            QiandaoItemBean qiandaoItemBean = qiandaoItemBeanList.get(var2);
+            CheckBox checkBox = (CheckBox) var1.getButton(R.id.qiandaoStatus_checkbok);
+            checkBox.setChecked(qiandaoItemBean.isQiandao());
+            TextView textView = (TextView)var1.getTextView(R.id.zhouji_textView);
+            textView.setText(qiandaoItemBean.getDay()+"");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        int size = qiandaoItemBeanList ==null?0: qiandaoItemBeanList.size();
+        Log.e(getClass().getName(), "getItemCount() called size="+size);
+        return size;
+//        return 4;
+    }
+
+
+}
---
+++ shanqianbao/src/main/java/com/shanqb/haoyi/tabview/BaseFragment.java
@@ -0,0 +1,130 @@
+package com.shanqb.haoyi.tabview;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.RequestQueue;
+import com.shanqb.haoyi.R;
+import com.shanqb.haoyi.inter.MyQueueResponse;
+import com.shanqb.haoyi.utils.MyVolleyUtils;
+import com.shanqb.haoyi.utils.NetworkUtils;
+import com.shanqb.haoyi.utils.XToastUtils;
+import com.shanqb.haoyi.view.LoadingProgressDialog;
+
+import java.util.Map;
+
+/**
+ * Created by yx on 16/4/3.
+ */
+public abstract class BaseFragment extends Fragment{
+
+    public MyVolleyUtils myVolley = null;
+    private LoadingProgressDialog mLoadingProgressDialog;
+    protected boolean isViewInitiated;
+    protected boolean isVisibleToUser;
+    protected boolean isDataInitiated;
+    public RequestQueue mQueue = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLoadingProgressDialog = new LoadingProgressDialog(getContext());
+        myVolley = new MyVolleyUtils(getActivity());
+        mQueue  = myVolley.getmQueue();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isViewInitiated = true;
+        prepareFetchData();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        this.isVisibleToUser = isVisibleToUser;
+        prepareFetchData();
+    }
+
+    public abstract void fetchData();
+
+    public boolean prepareFetchData() {
+        return prepareFetchData(false);
+    }
+
+    public boolean prepareFetchData(boolean forceUpdate) {
+        if (isVisibleToUser && isViewInitiated && (!isDataInitiated || forceUpdate)) {
+            fetchData();
+            isDataInitiated = true;
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        dismissLoadingDialog();
+        mLoadingProgressDialog = null;
+    }
+
+    /**
+     * 显示dialog
+     */
+    protected void showLoadingDialog() {
+        if (mLoadingProgressDialog != null && !mLoadingProgressDialog.isShowing()) {
+            mLoadingProgressDialog.show();
+        }
+    }
+
+    /**
+     * 关闭dialog
+     */
+    protected void dismissLoadingDialog() {
+        if (mLoadingProgressDialog != null && mLoadingProgressDialog.isShowing()) {
+            mLoadingProgressDialog.dismiss();
+        }
+    }
+
+
+    protected void requestPostQueue(boolean isShowDialog,String requestAction, Map<String, String> requestParams, MyQueueResponse myQueueResponse){
+        try {
+            if (!NetworkUtils.checkNetworkConnectionState(getContext())) {//未连接到网络
+                XToastUtils.toast(getString(R.string.net_close));
+                return;
+            }
+            if (isShowDialog){
+                showLoadingDialog();
+            }
+
+            myVolley.requestPostQueue(requestAction,requestParams,myQueueResponse);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            dismissLoadingDialog();
+        }
+    }
+
+    protected void requestGetQueue(boolean isShowDialog,String requestUrl, MyQueueResponse myQueueResponse){
+        try {
+            if (!NetworkUtils.checkNetworkConnectionState(getContext())) {//未连接到网络
+                XToastUtils.toast(getString(R.string.net_close));
+                return;
+            }
+            if (isShowDialog){
+                showLoadingDialog();
+            }
+
+            myVolley.requestGetQueue(requestUrl,myQueueResponse);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            dismissLoadingDialog();
+        }
+    }
+
+
+
+}
